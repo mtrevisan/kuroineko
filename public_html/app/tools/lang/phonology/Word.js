78b11519f2f7535c136916535ee3a2f5992b4dcc
mauro <mauro>
2017-05-09 22:41:01 +0200

    no message

--- public_html/app/tools/lang/phonology/Word.js
+++ public_html/app/tools/lang/phonology/Word.js
@@ -90,7 +90,7 @@ define(['tools/data/StringHelper', 'tools/lang/phonology/Grapheme', 'tools/lang/
 					&& !word.match(/^(re)?\(?g?\)?(à\/è|à|é|ò)[oaie]?$/)
 					&& !word.match(/^\(?x?\)?é$|^s[éí][oaie]?$/)
 					&& !word.match(/^((r[ei])?d[àé]|(kon(tra)?|[lƚ]iku[ei]|putre|rare|r[ei]|sora|stra|stupe|tore|tume)?f[àé]|(mal|move|soto)?st[àé])[oaie]?$/)
-					&& !word.match(/^v[àé]$/)
+					&& !word.match(/^v[àé](tu)?$/)
 					/*&& !word.match(/^s[éí]$/)*/
 					&& !word.match(/^(|as?|des?|es|kon|pro|re|so)tr[àé][oaie]?$/)?
 				word.replace(/[àéíóú]/g, function(chr){ return 'aeiou'['àéíóú'.indexOf(chr)]; }): word);
