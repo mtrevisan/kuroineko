26af5c6bce6297010236a1e32c584fb2d3b30993
Mauro Trevisan <mauro.simioni@targaubiest.com>
2021-09-29 09:07:34 +0200

    589

--- public_html/app/tools/lang/data/VerbsDictionary.js
+++ public_html/app/tools/lang/data/VerbsDictionary.js
@@ -4291,6 +4291,7 @@ define(function(){return [7, "radix", "prefix", "conjugation", "stressIdx", "str
 "martel",		"",			1,	4,	"g",	"1867",			"",
 "martel",		"x",			1,	4,	"g",	"1560",			"",
 "marteriđ",		"",			1,	6,	"a",	"1479",			"Martoriđar, martuređar.",
+"martiriđ",		"",			1,	6,	"a",	"1747",			"(veneŧian) Martoriđar, martuređar.",
 "martori",		"",			1,	4,	"g",	"2012",			"(arsedexe) Marteriđar, martuređar.",
 "martoriđ",		"",			1,	6,	"a",	"1854",			"Marteriđar, martuređar.",
 "martuređ",		"",			1,	6,	"a",	"",				"",
