d2a0eb8fc3ccbcc0d884ef6c03609ecd52846a0c
Mauro Trevisan <mauro.simioni@targaubiest.com>
2022-02-09 15:11:05 +0100

    15

--- public_html/app/tools/lang/data/VerbsDictionary.js
+++ public_html/app/tools/lang/data/VerbsDictionary.js
@@ -5002,6 +5002,7 @@ define(function(){return [7, "radix", "prefix", "conjugation", "stressIdx", "str
 "net",			"dex",		1,	1,	"a",	"1867",			"",
 "net",			"x",			1,	1,	"a",	"1775",			"",
 "netr",			"",			4,	4,	"a",	"1683",			"(veneŧian) ir. Nitrir.",
+"neŧesit",		"",			1,	3,	"g",	"1670",			"(veneŧian)",
 "nev",			"",			1,	1,	"a",	"1829",			"",
 "neveg",			"",			1,	1,	"a",	"1560",			"",
 "never",			"",			1,	1,	"a",	"1829",			"",
